90f101352242a424b247e95000184fed5ad84807
yunmonkey <yts0646@naver.com>
2018-08-02 18:24:23 +0900

    loginMake(진행중)
    loginMake1일차

---
+++ LoginMake/app/src/main/java/com/example/taeandroid/loginmake/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.taeandroid.loginmake
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
